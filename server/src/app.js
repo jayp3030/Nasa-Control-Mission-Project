af88e474eea426270637af66c551e5807ef488c8
jay <jayp33233@gmail.com>
2023-02-12 13:25:03 +0530

    POST/launches -> creating launches 2

--- server/src/app.js
+++ server/src/app.js
@@ -15,9 +15,13 @@ app.use(cors({
 app.use(morgan('combined'));
 
 app.use(express.json());
+
 app.use(express.static(path.join(__dirname,'..','public ')));
-app.use(planetsRouter);
-app.use(launchesRouter);
+
+app.use('/planets' , planetsRouter);
+
+app.use( '/launches' , launchesRouter);
+
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname,"..","public ","index.html"));
 });
