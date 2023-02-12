7e3c198d0334842eb5a0ed438891adcafe63620a
jay <jayp33233@gmail.com>
2023-02-12 10:53:28 +0530

    logged the request data coming in using morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
 
@@ -9,6 +10,9 @@ const app = express();
 app.use(cors({
     origin : 'http://localhost:3000'
 }));
+
+app.use(morgan('combined'));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'..','public ')));
 app.use(planetsRouter);
