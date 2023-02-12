bf4df5387e11ca3bfdd7f19719b6c38c67993b31
jay <jayp33233@gmail.com>
2023-02-12 11:21:35 +0530

    launches model added

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
 
 const app = express();
 
