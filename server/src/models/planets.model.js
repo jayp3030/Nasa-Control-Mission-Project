6f1a26b5564548fef35ab3c3693c4a85539f4841
jay <jayp33233@gmail.com>
2023-02-14 19:05:10 +0530

    planets data stored to database

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,50 +1,72 @@
-const {parse} = require('csv-parse');
-const fs = require('fs');
-const path = require('path');
+const { parse } = require("csv-parse");
+const fs = require("fs");
+const path = require("path");
 
-const planets = require('./planets.mongo');
-
-const habitablePlanets = [];
+const planets = require("./planets.mongo");
 
 function isHabitablePlanet(planet) {
-  return planet['koi_disposition'] === 'CONFIRMED'
-    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
-    && planet['koi_prad'] < 1.6;
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
 }
-function loadPlanets(){
-
-    return new Promise ( (resolve , reject) =>{
-        fs.createReadStream(path.join(__dirname,'..','..','data','kepler_data.csv'))
-      .pipe(parse({
-        comment: '#',
-        columns: true,
-      }))
-      .on('data', async (data) => {
+function loadPlanets() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(
+      path.join(__dirname, "..", "..", "data", "kepler_data.csv")
+    )
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", async (data) => {
         if (isHabitablePlanet(data)) {
-          await planets.create({
-            keplerName : data.kepler_name,
-          });
+          savePlanet(data);
         }
       })
 
-      .on('error', (err) => {
+      .on("error", (err) => {
         console.log(err);
         reject(err);
       })
 
-      .on('end', () => {
-        console.log(`${habitablePlanets.length} habitable planets found!`);
+      .on("end", async () => {
+        const countPlanetFound = (await getAllPlanets()).length;
+        console.log(`${countPlanetFound} habitable planets found!`);
         resolve();
       });
-
-    });  
+  });
 }
 
-function getAllPlanets(){
-  return habitablePlanets;
+async function getAllPlanets() {
+  return await planets.find({} , {
+    '_id':0 , '__v':0,
+  });
+}
+
+async function savePlanet(planet) {
+  try {
+    await planets.updateOne(
+      {
+        keplerName: planet.kepler_name,
+      },
+      {
+        keplerName: planet.kepler_name,
+      },
+      {
+        upsert: true,
+      }
+    );
+  } catch (err) {
+    console.log(`could not save planet : ${err}`);
+  }
 }
 
 module.exports = {
-    loadPlanets,
-    getAllPlanets,
+  loadPlanets,
+  getAllPlanets,
 };
